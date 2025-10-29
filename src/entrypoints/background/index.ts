c0c69e5c5bb387238a1db4d870c54a71b56ef422
VoidsAreThere65715 <t19.thakur@outlook.com>
2025-10-29 19:09:42 +0530

    refactor: Refactor the code for better readability and maintainability
    This commit focuses on improving the code based on readability,
    maintainibility and performance.
    
    Key Changes include:
    * Removed console.log messages
    * Added types for better readabilty and maintanibility

--- src/entrypoints/background/index.ts
+++ src/entrypoints/background/index.ts
@@ -1,12 +1,15 @@
+import { defineBackground } from "wxt/utils/define-background";
+
 export default defineBackground(() => {
-  browser.runtime.onMessage.addListener((message, sender, sendResponse) => {
-    if (message.type === "comments") {
-      console.log("Scraped comments:", message.comments);
-      // Optionally, you can store the data in localStorage or sync storage
-      localStorage.setItem(
-        "youtube_comments",
-        JSON.stringify(message.comments),
-      );
-    }
-  });
+	browser.runtime.onMessage.addListener(
+		(message: any, _sender: any, _sendResponse: any) => {
+			if (message.type === "comments") {
+				console.log("Scraped comments:", message.comments);
+				localStorage.setItem(
+					"youtube_comments",
+					JSON.stringify(message.comments),
+				);
+			}
+		},
+	);
 });

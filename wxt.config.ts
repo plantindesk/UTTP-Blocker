3f06b5d6375a7f9b3b3f1018ad2def6e007e178f
VoidsAreThere65715 <t19.thakur@outlook.com>
2025-10-29 19:33:06 +0530

    refactor: Remove unused tailwind and react configuration
    The project does not need any popup entrypoints, and so there is no need
    for tailwind and react configuration.
    
    Key Changes includes:
    - Removed import statement of tailwindcss/vite
    - Removed block used for making vite render tailwindcss
    - Removed import of react in module configuration

--- wxt.config.ts
+++ wxt.config.ts
@@ -1,11 +1,9 @@
-import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "wxt";
 export default defineConfig({
-	vite: () => ({
-		plugins: [tailwindcss()],
-	}),
-	modules: ["@wxt-dev/module-react"],
 	// Relative to project root
 	srcDir: "src", // default: "."
 	outDir: "dist", // default: ".output"
+	manifest: {
+		name: "UTTP Blocker",
+	},
 });

e0bab7c700d18bc0c8c6bd94f4d68c3ee8bd4e21
VoidsAreThere65715 <t19.thakur@outlook.com>
2025-11-15 14:10:44 +0530

    feat: Add no collection in permission for firefox related extension
    I have added properties on manifest that specifies that the extension
    doesn't collect any data. This property is required for firefox based
    extensions

--- wxt.config.ts
+++ wxt.config.ts
@@ -1,9 +1,19 @@
 import { defineConfig } from "wxt";
+
 export default defineConfig({
 	// Relative to project root
 	srcDir: "src", // default: "."
 	outDir: "dist", // default: ".output"
 	manifest: {
 		name: "UTTP Blocker",
+		browser_specific_settings: {
+			gecko: {
+				id:
+					process.env.FIREFOX_EXTENSION_ID
+				data_collection_permissions: {
+					required: ["none"],
+				},
+			},
+		},
 	},
 });
